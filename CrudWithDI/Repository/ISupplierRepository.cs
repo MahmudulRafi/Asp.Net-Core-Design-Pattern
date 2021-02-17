75863ae70573474d2cc8c2731361b21699f864a3
MahmudulRafi <rafi.mahmudul.cse@gmail.com>
2021-02-18 00:54:59 +0600

    core crud using DI final commit

---
+++ CrudWithDI/Repository/ISupplierRepository.cs
@@ -0,0 +1,17 @@
+﻿using CrudWithDI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudWithDI.Repository
+{
+        public interface ISupplierRepository
+        {
+            public Task<List<Supplier>> GetSuppliers();
+            public Task<Supplier> GetSupplier(int? id);
+
+            public Task Save(Supplier supplier);
+
+        }
+}
---
+++ CrudWithDI/Services/SupplierService.cs
@@ -0,0 +1,34 @@
+﻿using CrudWithDI.Models;
+using CrudWithDI.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudWithDI.Services
+{
+    public class SupplierService : ISupplierService
+    {
+        private readonly ISupplierRepository _repository;
+
+        public SupplierService(ISupplierRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<Supplier>> GetSuppliers()
+        {
+            return await _repository.GetSuppliers();
+        }
+
+        public async Task<Supplier> GetSupplier(int? id)
+        {
+            return await _repository.GetSupplier(id);
+        }
+
+        public async Task Add(Supplier supplier)
+        {
+            await _repository.Save(supplier);
+        }
+    }
+}
